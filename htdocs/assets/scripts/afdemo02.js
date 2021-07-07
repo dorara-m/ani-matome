65a17d62f7ae405f2974404fdeb50fa6f326203f
dorara@work <satosinisemono@gmail.com>
2021-07-07 11:14:37 +0900

    add: [af02] 途中

---
+++ htdocs/assets/scripts/afdemo02.js
@@ -0,0 +1,13 @@
+var params = {
+  container: document.getElementById('lottie'),
+  // アニメーションをsvg形式で出力
+  renderer: 'svg',
+  // アニメーションをループする
+  loop: true,
+  // アニメーションを読み込んだら自動で再生する
+  autoplay: true,
+  // アニメーションファイル(.json)のパス
+  path: "../assets/json/afdemo02_data.json"
+}; // アニメーションをロード
+
+var anim = lottie.loadAnimation(params);
