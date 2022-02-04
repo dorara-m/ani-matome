af593eb699190e50d6e45169959cf75e8d17e8b1
dorara@work <satosinisemono@gmail.com>
2022-02-04 12:49:59 +0900

    update: おぐたま機能追加

--- src/js/assets/js/script.ts
+++ src/js/assets/js/script.ts
@@ -11,6 +11,15 @@ tabs.on('click', function () {
   $('#js-cnt > li').eq(index).addClass('active')
 })
 
+const wrapDom = document.getElementById('js-note-wrap')
+const overlayDom = document.getElementById('js-note-overlay')
+overlayDom.addEventListener('click', function(){
+  wrapDom.classList.add('none')
+})
+document.getElementById('js-note-ballon').addEventListener('click', function(){
+  wrapDom.classList.remove('none')
+})
+
 //pages
 $('#js-reload').on('click', function () {
   location.reload()
