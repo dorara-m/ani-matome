fd38624790a2d361ed7a32cf37ba53817a667d82
dorara <satosinisemono@gmail.com>
2022-02-12 12:45:10 +0900

    uma更新02

--- src/js/assets/js/script.ts
+++ src/js/assets/js/script.ts
@@ -12,6 +12,7 @@ tabs.on('click', function () {
 })
 
 //top - releaseNote
+// チャット画面モーダルのon/off
 const wrapDom = document.getElementById('js-note-wrap')
 const overlayDom = document.getElementById('js-note-overlay')
 const ballonDom =  document.getElementById('js-note-ballon')
@@ -26,7 +27,46 @@ if (overlayDom && wrapDom) {
   }
 }
 
+// チャット一覧の内容描画
 const listDom = document.getElementById('js-note-list') || null
+
+const removeBr = (str: string) => {
+  if (str.indexOf('<br>') == -1) {
+    return str
+  }
+  const replaced = str.replace('<br>', '')
+  return replaced
+}
+const trimString = (str: string, max_length: number) => {
+  if (str.length <= max_length) {
+    return str
+  }
+  let trimedStr = str.substr(0, max_length) + '…'
+  return trimedStr
+}
+function makeListHTML() {
+  const json = require('./releaseNote.json')
+  const list = json.releaseNote
+  let html = '<ul>'
+  list.forEach((item:any) => {
+    html += `<li class="js-note-item">
+    <div class="icon">
+    <img src="/assets/img/${item.chats[item.chats.length - 1].who}.jpg" alt="" />
+    </div>
+    <div class="text">
+    <div class="head">${item.date}</div>
+    <p>${trimString(removeBr(item.chats[item.chats.length - 1].says), 18)}</p>
+    </div>
+    <div class="count unread">${item.chats.length}</div>
+    </li>`
+  })
+  html += '</ul>'
+  listDom.innerHTML = html
+}
+makeListHTML()
+
+
+// チャット一覧-詳細の移動
 const pageDom = document.getElementById('js-note-page') || null
 const backDom = document.getElementById('js-note-back') || null
 const listItemsDom = document.getElementsByClassName('js-note-item')
@@ -49,7 +89,7 @@ const returnChat = (listNum:any) => {
     "tama": "タマモクロス"
   }
   const json = require('./releaseNote.json')
-  const notes = json.releaseNote[listNum]
+  const notes = json.releaseNote[listNum].chats
 
   let html = ''
   notes.forEach((note:any) => {
@@ -69,6 +109,8 @@ const returnChat = (listNum:any) => {
   });
   document.getElementById("js-note-chat").innerHTML = html
 }
+
+
 //pages
 $('#js-reload').on('click', function () {
   location.reload()
