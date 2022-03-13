30dc0fe5231aafa17dcc0797e3ef94258792a3d3
dorara@work <satosinisemono@gmail.com>
2022-03-13 22:35:44 +0900

    update: uma-cms

--- src/js/assets/js/script.ts
+++ src/js/assets/js/script.ts
@@ -1,51 +1,13 @@
-// microCMS接続処理
-const fetchCMS = () => {
-  fetch(
-    "https://rqfoifxr3x.microcms.io/api/v1/ani-links",
-    {
-      headers: {
-        "X-MICROCMS-API-KEY": "309375b1533b47f4b56d85202171276bf164"
-      }
-    })
-  .then(res => res.json())
-  .then(json => {
-    const cmsDataArray = json.contents
-    makeLinksTable(cmsDataArray)
-  })
-}
-// fetchCMS()
-
-const makeLinksTable = (array) => {
-  const tableDom = document.getElementById("js-links-table")
-  if (!tableDom) return
-  let html = ''
-  array.forEach(item => {
-    html += `<tr>
-    <th><a href="${item.link}">${item.title}</a></th>`
-    if (item.sample) {
-      html += `<td><a href="${item.sample}">あり</a></td>`
-    } else {
-      html += '<td>-</td>'
-    }
-    html += `</tr>`
-  })
-  tableDom.innerHTML = html
-}
-
-
-//共通要素 - releaseNote
-// 最初にjsonを読みにいく。
-const json = require('./releaseNote.json')
-// 未読フラグ機能
 // 未読フラグデータが入ってくる用の配列
 let releaseNoteFlags
+
 // まず未読フラグがストレージにないかチェック
-const checkLocalStorage = () => {
+const checkLocalStorage = (data: any) => {
   const readFlag = localStorage.getItem('readFlag')
   if (!readFlag) {
     // なし(初回)なら全部未読の配列を作成
     let flags = [], i = 0
-    while (i < json.releaseNote.length) {
+    while (i < data.length) {
       flags.push(false)
       i++
     }
@@ -56,9 +18,9 @@ const checkLocalStorage = () => {
     // 一時変数
     const tmpFlags = JSON.parse(readFlag)
     // もしこのフラグがjsonデータと一致しない場合…（つまりチャット数が増えていた場合
-    if (tmpFlags.length < json.releaseNote.length) {
+    if (tmpFlags.length < data.length) {
       // その差分だけ配列頭にfalseを足す
-      for (let i=0; i < json.releaseNote.length - tmpFlags.length; i++) {
+      for (let i=0; i < data.length - tmpFlags.length; i++) {
         tmpFlags.unshift(false)
       }
       // console.log(tmpFlags)
@@ -67,37 +29,18 @@ const checkLocalStorage = () => {
     releaseNoteFlags = tmpFlags
   }
 }
-// 未読フラグを画面下バルーンに反映
-const ballonIsRead = () => {
-  const ballonDom =  document.getElementById('js-note-ballon')
-  if (releaseNoteFlags.indexOf(false) !== -1) {
-    ballonDom.classList.add('unRead')
-  } else {
-    ballonDom.classList.remove('unRead')
+
+const toggleChat = () => {
+  const wrapDom = document.getElementById('js-note-wrap')
+  const toggleNoteDom = document.getElementById('js-note-toggle')
+
+  if (!toggleNoteDom || !wrapDom) {
+    return
   }
-}
-
-checkLocalStorage()
-// ballonIsRead()
-
-
-// チャット画面のtoggle
-const wrapDom = document.getElementById('js-note-wrap')
-const toggleNoteDom = document.getElementById('js-note-toggle')
-const closeNoteDoms = document.getElementsByClassName('js-note-close')
-// toggle
-toggleNoteDom.addEventListener('click', ()=> {
-  wrapDom.classList.toggle('-js-active')
-})
-// off
-for (let i=0; i<closeNoteDoms.length; i++) {
-  closeNoteDoms[i].addEventListener('click', function(){
-    wrapDom.classList.remove('-js-active')
+  toggleNoteDom.addEventListener('click', ()=> {
+    wrapDom.classList.toggle('-js-active')
   })
 }
-
-// チャット一覧の内容描画
-const listContentDom = document.getElementById('js-note-listContent') || null
 
 const removeBr = (str: string) => {
   if (str.indexOf('<br>') == -1) {
@@ -106,6 +49,7 @@ const removeBr = (str: string) => {
   const replaced = str.replace('<br>', '')
   return replaced
 }
+
 const trimString = (str: string, max_length: number) => {
   if (str.length <= max_length) {
     return str
@@ -113,8 +57,18 @@ const trimString = (str: string, max_length: number) => {
   let trimedStr = str.substr(0, max_length) + '…'
   return trimedStr
 }
-function makeListHTML() {
-  const list = json.releaseNote
+
+const formatDate = (date: string) => {
+  // 仮で頭10文字とるだけに
+  // @todo ちゃんと日付判定して表記を3030.03.01にする
+  return date.substr(0, 10)
+}
+
+const makeListHTML = (data: any) => {
+  const listContentDom = document.getElementById('js-note-listContent')
+  if (!listContentDom) return
+
+  const list = data
   let html = '<ul>'
   for(let i=0; i<list.length; i++) {
     html += `<li class="js-note-item">
@@ -122,7 +76,7 @@ function makeListHTML() {
     <img src="/assets/img/${list[i].chats[0].who}.jpg" alt="" />
     </div>
     <div class="text">
-    <div class="head">${list[i].date}</div>
+    <div class="head">${formatDate(list[i].date)}</div>
     <p>${trimString(removeBr(list[i].chats[0].says), 13)}</p>
     </div>
     <div class="count js-count ${releaseNoteFlags[i] ? '' : 'unRead'}">${list[i].chats.length}</div>
@@ -131,56 +85,17 @@ function makeListHTML() {
   html += '</ul>'
   listContentDom.innerHTML = html
 }
-makeListHTML()
 
-
-// チャット一覧-詳細の移動
-const listDom = document.getElementById('js-note-list') || null
-const pageDom = document.getElementById('js-note-page') || null
-const backDom = document.getElementById('js-note-back') || null
-const listItemsDom = document.getElementsByClassName('js-note-item')
-for(let i = 0; i < listItemsDom.length; i++) {
-  listItemsDom[i].addEventListener('click', function(){
-    listDom.classList.add('slideOut')
-    pageDom.classList.add('slideIn')
-    // ここでスクロールリセット
-    pageDom.scrollTo(0, 0)
-    makeBallons(i)
-
-    // ここでフラグを切り替える
-    if (!releaseNoteFlags[i]) {
-      releaseNoteFlags[i] = true
-      // フラグの切り替えを生成済みのhtmlに反映する
-      // バルーン
-      // ballonIsRead()
-      // リスト
-      const countDoms = document.getElementsByClassName('js-count')
-      for (let i=0; i<countDoms.length; i++) {
-        if (releaseNoteFlags[i]) {
-          countDoms[i].classList.remove('unRead')
-        }
-      }
-      // localStrageにも反映しとく
-      localStorage.setItem('readFlag', JSON.stringify(releaseNoteFlags))
-    }
-  })
-}
-
-backDom.addEventListener('click', function(){
-  listDom.classList.remove('slideOut')
-  pageDom.classList.remove('slideIn')
-})
-
-const makeBallons = (listNum:any) => {
+const makeBallons = (listNum: any, data: any) => {
   const uma = {
     "oguri": "オグリキャップ",
     "tama": "タマモクロス",
     "chiyo": "サクラチヨノオー"
   }
-  const numberOfUma = json.releaseNote[listNum].person
+  const numberOfUma = data[listNum].person
   document.getElementById("js-note-number").innerHTML = numberOfUma
 
-  const notes = json.releaseNote[listNum].chats
+  const notes = data[listNum].chats
 
   let html = ''
   notes.forEach((note:any) => {
@@ -192,15 +107,84 @@ const makeBallons = (listNum:any) => {
     <div class="name">${uma[note.who]}</div>`
     if (note.says) {
       html += `<div class="says">${note.says}</div>`
-    } else if (note.links) {
-      html += `<a class="says" href="${note.link}">${note.link}</a>`
     } else {
-      html += `<a href="/assets/img/${note.image}" class="saysImage"><img src="/assets/img/${note.image}", alt=""></a>`
+      html += `<a href="${note.image.url}" class="saysImage"><img src="${note.image.url}", alt=""></a>`
     }
     html += `</div></li>`
   });
   document.getElementById("js-note-chat").innerHTML = html
 }
+
+const moveListToDetail = (data: any) => {
+  const listDom = document.getElementById('js-note-list')
+  const pageDom = document.getElementById('js-note-page')
+  const backDom = document.getElementById('js-note-back')
+  const listItemsDom = document.getElementsByClassName('js-note-item')
+
+  if (!listDom || !pageDom || !backDom) {
+    return false
+  }
+  
+  // 一覧アイテムを押したとき詳細へ
+  for(let i = 0; i < listItemsDom.length; i++) {
+    listItemsDom[i].addEventListener('click', function(){
+      listDom.classList.add('slideOut')
+      pageDom.classList.add('slideIn')
+      // ここでスクロールリセット
+      pageDom.scrollTo(0, 0)
+      makeBallons(i, data)
+  
+      // ここでフラグを切り替える
+      if (!releaseNoteFlags[i]) {
+        releaseNoteFlags[i] = true
+        // フラグの切り替えを生成済みのhtmlに反映する
+        // バルーン
+        // ballonIsRead()
+        // リスト
+        const countDoms = document.getElementsByClassName('js-count')
+        for (let i=0; i<countDoms.length; i++) {
+          if (releaseNoteFlags[i]) {
+            countDoms[i].classList.remove('unRead')
+          }
+        }
+        // localStrageにも反映しとく
+        localStorage.setItem('readFlag', JSON.stringify(releaseNoteFlags))
+      }
+    })
+  }
+  // 詳細から一覧へ
+  backDom.addEventListener('click', function(){
+    listDom.classList.remove('slideOut')
+    pageDom.classList.remove('slideIn')
+  })
+}
+
+
+async function init() {
+  const res = await fetch(
+  "https://rqfoifxr3x.microcms.io/api/v1/release-note",
+  {
+    headers: {
+      "X-MICROCMS-API-KEY": "309375b1533b47f4b56d85202171276bf164"
+    }
+  })
+  const json = await res.json()
+  const cmsData = await json.contents
+  console.log(cmsData)
+
+  checkLocalStorage(cmsData)
+  
+  // チャット画面のtoggle
+  toggleChat()
+
+  // チャット一覧の内容描画
+  makeListHTML(cmsData)
+  
+  // チャット一覧-詳細の移動
+  moveListToDetail(cmsData)
+}
+init()
+
 
 
 //pages
