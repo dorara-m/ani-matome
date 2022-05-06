7b31da7b92f4fbdf2ada82d0d7c362e580800c2b
dorara@work <satosinisemono@gmail.com>
2022-05-06 11:24:54 +0900

    update: chara-anime

--- src/js/assets/js/pages/charaIntro.ts
+++ src/js/assets/js/pages/charaIntro.ts
@@ -6,9 +6,18 @@ const charaIntro = () => {
         // ここでセリフをセクションごとに対応したものに変えれば良い。
         // 書くセクションにセットしておいたセリフを呼び出し
         // console.log(entry.target.getAttribute("data-ballon"))
+        const animeDom = document.querySelector('.fixedAnime')
         const ballonDom = document.getElementById("chara-ballon")
-        if (!ballonDom) return
-        ballonDom.innerHTML = entry.target.getAttribute("data-ballon")
+        if (!ballonDom || !animeDom) return
+
+        const text = entry.target.getAttribute("data-ballon")
+        // セリフ区間を終えるための要素をトリガー対象に含めておき、その要素に入ったら要素を隠すように
+        if (text === 'end') {
+          animeDom.classList.add('-hide')
+        } else {
+          animeDom.classList.remove('-hide')
+          ballonDom.innerHTML = text
+        }
       }
     })
   }
