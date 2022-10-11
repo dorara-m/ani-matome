1ed12f489091649b6bc3e7506c14b834ef649359
dorara@work <satosinisemono@gmail.com>
2022-10-11 16:32:31 +0900

    update

--- src/js/assets/js/pages/gsapImage01.ts
+++ src/js/assets/js/pages/gsapImage01.ts
@@ -1,17 +1,16 @@
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-console.log('hello gsap!')
 gsap.registerPlugin(ScrollTrigger)
 gsap
   .timeline({
     scrollTrigger: {
-      trigger: '.before',
-      start: 'top top',
-      end: 'bottom+=200px top',
+      trigger: '.target',
+      start: 'top center',
+      end: 'bottom center',
       toggleActions: 'play none none reset',
       markers: true,
-      scrub: true,
+      scrub: 0.8,
     },
   })
   .from('.targetImg', {
