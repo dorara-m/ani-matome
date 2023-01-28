24f59d8a6945e4d16fb630cbf2c6c0433898d961
kinjo <kinjo.tam@tam-tam.co.jp>
2023-01-28 22:50:08 +0900

    mod: 動きをGSAPに置き換え

--- src/js/assets/js/pages/cyberpunk.ts
+++ src/js/assets/js/pages/cyberpunk.ts
@@ -1,11 +1,19 @@
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-const blockEl = document.querySelector('.wrap')!;
-const coefPram = getComputedStyle(blockEl);
-console.log(coefPram)
+gsap.registerPlugin(ScrollTrigger)
 
-window.addEventListener('scroll', () => {
-  let scrollPos = window.pageYOffset;
-  const coefStyle = coefPram.getPropertyValue("--coef-base-width");
-  document.documentElement.style.setProperty('--coef-base-width',String(scrollPos*0.001));
-  console.log(coefStyle)
+const blockAnimation = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".wrap",
+    start: 'top 100%',
+    end: 'bottom 20%',
+    scrub: true,
+    markers: true,
+  }
 })
+blockAnimation.fromTo(".block1", { width: '50vw' }, { width: "70vw" });
+blockAnimation.fromTo(".block2", { width: '35vw' }, { width: "55vw" });
+blockAnimation.fromTo(".text", { x: '25vw' }, { x: "35vw" });
+blockAnimation.fromTo(".block3", { width: '20vw' }, { width: "30vw" });
+blockAnimation.fromTo(".block4", { width: '40vw' }, { width: "60vw" });
