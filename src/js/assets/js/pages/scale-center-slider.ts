bcbcdf1bc700ccbfd3db301e3fcf40d41bcda325
dorara@work <satosinisemono@gmail.com>
2023-04-07 19:44:11 +0900

    update: [scale-center-slider]

--- src/js/assets/js/pages/scale-center-slider.ts
+++ src/js/assets/js/pages/scale-center-slider.ts
@@ -4,7 +4,8 @@ import Swiper, { Navigation } from 'swiper'
 // いつかできるようにしたい↓いまはそれ専用の入力欄を増やして対応
 // import 'swiper/css'
 
-const swiper = new Swiper('.swiper-container', {
+// ここで与えるクラス名はuniqueにすること、じゃないと動きが干渉して正常に動かない
+const swiper = new Swiper('.swiper-container1', {
   modules: [Navigation],
   centeredSlides: true,
   loop: true,
@@ -33,21 +34,11 @@ const swiper2 = new Swiper('.swiper-container2', {
   centeredSlides: true,
   loop: true,
   loopedSlides: 2,
-  speed: 500,
-  slidesPerView: 1.5,
-  // スマホのときだけスライド間隔を小さく
-  spaceBetween: 40,
-  autoplay: {
-    delay: 3000,
-  },
+  speed: 1000,
+  slidesPerView: 1.8,
+  spaceBetween: 67,
   navigation: {
     nextEl: '.swiper-button-next2',
     prevEl: '.swiper-button-prev2',
-  },
-  breakpoints: {
-    // pcのスライド間隔
-    768: {
-      spaceBetween: 80,
-    },
   },
 })
